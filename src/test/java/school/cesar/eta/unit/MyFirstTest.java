23e3da9cd2f6c4de3143beb4521f6129395a2467
Milton Castro <milton.castro@inovant.com.br>
2020-09-26 01:08:06 -0300

    My first test

---
+++ src/test/java/school/cesar/eta/unit/MyFirstTest.java
@@ -0,0 +1,13 @@
+package school.cesar.eta.unit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyFirstTest {
+
+    @Test
+    public void myVeryFirstTest(){
+        Assertions.assertTrue(true);
+    }
+
+}
